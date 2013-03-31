63adf38520e03a9e252d4d72919367a2af9c318f
Šime Vidas <sime.vidas@gmail.com>
2013-03-31 19:38:36 +0200

    Refactored the JavaScript a bit

--- js/daily.js
+++ js/daily.js
@@ -5,15 +5,21 @@ jQuery(function ( $ ) {
         $dataWrapper,   // the DOM element that wraps the HTML generated from MD
         $menu,          // the list of dailies in the top right of the page
         $article,       // the <article> element (i.e. the entire page)
-        data;           // the Markdown text (retrieved via Ajax)
+        data,           // the Markdown text (retrieved via Ajax)
+        jqXHR,          // the jQuery Ajax object used for retrieving MD files
 
-    // initializing the static references
-    $article = $( 'body' ).children( 'article:first' );
-    $dataWrapper = $( '.md-data:first' );
-    $menu = $( '.daily-menu:first' );
-    converter = new Markdown.Converter();
+        // functions (commented below)
+        processData,
+        createMenu;
 
-    $.get( '/data/latest.md', function ( data ) {
+
+    // request the data as soon as possible
+    jqXHR = $.get( '/data/latest.md' );
+
+
+    // converts the Markdown text to HTML and appends it to the page
+    // this is an Ajax response handler; "data" is Markdown text
+    processData = function ( data ) {
         var html = converter.makeHtml( data ),
             $temp = $( '<div></div>' ).html( html ); // serves as temporary document fragment (to hold the DOM tree generated from the MD file)
 
@@ -40,30 +46,38 @@ jQuery(function ( $ ) {
             }
         });
 
-        // quick access menu
-        (function () {
-            var $headers  = $temp.children( 'h2' ).slice( 1 );
-
-            $headers.map(function () {
-                var text = $( this ).text();
-                return $( '<li>' + text.slice( 0, text.indexOf( ',' ) ) + '</li>' )[0];
-            }).appendTo( $menu );
-
-            $menu.on( 'click', 'li', function () {
-                var index = $( this ).index();
-                var vPos = index === 0 ? 0 : $headers.eq( index - 1 ).position().top + 16;
-                scrollTo( 0, vPos );
-            });
-
-            $( window ).on( 'resize', function () {
-                $menu.css({ left: $article.offset().left + $article.outerWidth() + 16 });
-            }).triggerHandler( 'resize' );
-        }());
-
         $dataWrapper.append( $temp.children() );
+    };
 
-        // the content is displayed only at the end to avoid unnecessary re-flows
-        $dataWrapper.show();
-        $menu.show();
-    });
+
+    // populates the $menu element
+    createMenu = function () {
+        var $headers  = $dataWrapper.children( 'h2' ).slice( 1 );
+
+        $headers.map(function () {
+            var text = $( this ).text();
+            return $( '<li>' + text.slice( 0, text.indexOf( ',' ) ) + '</li>' )[0];
+        }).appendTo( $menu );
+
+        $menu.on( 'click', 'li', function () {
+            var index = $( this ).index();
+            var vPos = index === 0 ? 0 : $headers.eq( index - 1 ).position().top + 16;
+            scrollTo( 0, vPos );
+        });
+
+        $( window ).on( 'resize', function () {
+            $menu.css({ left: $article.offset().left + $article.outerWidth() + 16 });
+        }).triggerHandler( 'resize' );
+    };
+
+
+    // initialize the static references
+    $article = $( 'body' ).children( 'article:first' );
+    $dataWrapper = $( '.md-data:first' );
+    $menu = $( '.daily-menu:first' );
+    converter = new Markdown.Converter();
+
+
+    // hook up callbacks for the initial Ajax request
+    jqXHR.then( processData ).then( createMenu );
 });
