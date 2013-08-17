ca6a9c44d59d3111f53982cb0de99b2289bfdf49
Sime Vidas <sime.vidas@gmail.com>
2013-08-17 05:53:38 +0200

    Edited Grunt related files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,21 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			all: ['*.js']
 		},
 		uglify: {
-			dist: {
-				src: 'js/daily.js';
-				dest: 'js/daily.min.js'
+			options: {
+				banner: '/*! <%= pkg.name %>' +
+						' <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
+			dist: {
+				src: 'js/daily.js',
+				dest: 'js/daily.min.js'
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['jshint', 'uglify']);
 };
