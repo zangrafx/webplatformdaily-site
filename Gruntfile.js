899c82a57bbf5383f6e0429eadbde4a76046d8cf
Sime Vidas <sime.vidas@gmail.com>
2013-08-18 05:23:31 +0200

    Added and set up several Grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,31 @@
 module.exports = function (grunt) {
+	'use strict';
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		banner: '/*! <%= pkg.name %>' +
 				' <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'styles/daily.css': 'styles/daily.sass'
+				}
+			}
+		},
+		csslint: {
+			strict: {
+				// src: ['styles/daily.css'] // disabled by default
+			}
+		},
+		concat: {
+			dist: {
+				src: ['styles/reset/cssreset-min.css', 'styles/daily.css'],
+				dest: 'styles/daily.css'
+			}
+		},
 		jshint: {
 			options: {
 				curly: true,
@@ -47,11 +70,38 @@ module.exports = function (grunt) {
 					}	
 				}
 			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 9001
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			css: {
+				files: ['styles/daily.sass'],
+				tasks: ['css']
+			},
+			scripts: {
+				files: ['js/daily.js'],
+				tasks: ['js']
+			}
 		}
 	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-markdown');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('css', ['sass', 'csslint', 'concat']);
 	grunt.registerTask('js', ['jshint', 'uglify']);
 	grunt.registerTask('md', ['markdown']);
+	grunt.registerTask('server', ['connect', 'watch']);
 };
